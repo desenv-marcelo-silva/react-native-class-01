5edd92399d01ac21d7eadd2d92e667c63c899140
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-10 22:21:28 -0300

    Criação de páginas: Main e User.

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <View>
+      <Text>Primeira página</Text>
+      <Button title="Navigate to users" />
+    </View>
+  );
+}
