a8e2dacbd12c4339b5f7a26cec1d08e61b7b50f9
desenv-marcelo-silva <desenv.marcelo.silva@gmail.com>
2020-03-14 23:34:31 -0300

    Exibição dos detalhes de repositório com estrelas.

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Container, Header, Avatar, Name, Bio } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 import api from '../../services/api';
 
@@ -38,6 +50,20 @@ export default class User extends React.Component {
           <Name>{user.name}</Name>
           <Bio>{user.bio}</Bio>
         </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
       </Container>
     );
   }
